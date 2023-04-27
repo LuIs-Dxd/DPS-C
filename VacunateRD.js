7b6b502ae8e336f7519005984e1a0518bec8f9d4
LuIs-Dxd <45068622+LuIs-Dxd@users.noreply.github.com>
2023-04-27 13:34:48 -0400

    Create VacunateRD.js

---
+++ VacunateRD.js
@@ -0,0 +1,37 @@
+javascript:(function() {
+    function seleccionarDPS() {
+      const selectElement = document.querySelector('#dps');
+      if (selectElement) {
+        selectElement.value = 'SAN JUAN'; //<--- Selecciona tu DPS (pueden buscarlo desde f12 y recuerda eliminar este comentario).
+        const event = new Event('change', { bubbles: true });
+        selectElement.dispatchEvent(event);
+      }
+    }
+  
+    function Pbutton() {
+      const filtrarBtn = document.querySelector('.btn.btn-primary.btn-block');
+      if (filtrarBtn) {
+        filtrarBtn.click();
+      }
+    }
+  
+    function AccionKey(event) {
+      const tecla = 'X'; //<--- Tecla de accion y recuerda eliminar este comentario.
+      const elementoActivo = document.activeElement;
+  
+      if (
+        elementoActivo.tagName === 'INPUT' ||
+        elementoActivo.tagName === 'TEXTAREA' ||
+        elementoActivo.isContentEditable
+      ) {
+        return;
+      }
+  
+      if (event.key.toUpperCase() === tecla.toUpperCase()) {
+        seleccionarDPS();
+        Pbutton();
+      }
+    }
+  
+    document.addEventListener('keydown', AccionKey);
+  })();
